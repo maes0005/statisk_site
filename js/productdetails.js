3e06bf69616eb9f1fe99e16581e96b10bea7be83
Maja <maes0005@stud.ek.dk>
2026-03-03 09:18:22 +0100

    productdetails

---
+++ js/productdetails.js
@@ -0,0 +1,38 @@
+const fisk = new URLSearchParams(window.location.search).get("id");
+const endpoint = `https://kea-alt-del.dk/t7/api/products/${fisk}`;
+
+const container = document.querySelector("#productContainer");
+
+function getData() {
+  fetch(endpoint)
+    .then((res) => res.json())
+    .then(showData);
+}
+
+function showData(json) {
+  console.log(json);
+  container.innerHTML = `
+   <figure>
+        <img
+          src="https://kea-alt-del.dk/t7/images/webp/640/${json.id}.webp"
+          alt="Produktbillede"
+          class="productImage"
+        />
+        <span class="saleLabel"></span>
+      </figure>
+      <section class="productDetails">
+        <h2 class="productName">${json.productdisplayname}</h2>
+        <div>
+          <p class="articleType"><span class="bold">Type:</span> ${json.articletype}</p>
+          <p class="productCategory">
+            <span class="bold">Kategori:</span> ${json.category}
+          </p>
+          <p class="productPrice"><span class="bold">Pris:</span> ${json.price},-</p>
+          <p class="stock"><span class="bold">Stock:</span> ${json.soldout}</p>
+        </div>
+        <button class="buyButton">Køb nu</button>
+      </section>
+  `;
+}
+
+getData();
